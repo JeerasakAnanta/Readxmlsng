a9f7c83ce4f0295db392e99f0876396095824b1c
Game <jeerasakananta@gmail.com>
2024-09-28 23:17:56 +0700

    update : CORSMiddleware

--- api.py
+++ api.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 from pydantic import BaseModel
 
